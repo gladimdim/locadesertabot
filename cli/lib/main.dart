c693ace2183de44c27b4fcd8e6ee0c1d2e434d38
Dmytro Gladkyi <gladimdim@gmail.com>
2020-07-28 16:24:41 +0300

    Printing.

--- cli/lib/main.dart
+++ cli/lib/main.dart
@@ -5,7 +5,7 @@ import 'package:locadesertabot/bot/server.dart';
 void main() async {
   final envVars = Platform.environment;
   final bot_token = envVars['BOT_TOKEN'];
-
+  print("Bot token: $bot_token");
   final server = Server(bot_token);
 
   server.startBot();
