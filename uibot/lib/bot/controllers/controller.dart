807c9ee2cc5193bb880eb9e6f3837c9c69213f9b
Dmytro Gladkyi <gladimdim@gmail.com>
2020-09-22 16:11:10 +0300

    Added dot.

--- uibot/lib/bot/controllers/controller.dart
+++ uibot/lib/bot/controllers/controller.dart
@@ -20,7 +20,7 @@ class Controller {
 
   processListStories(Message message) async {
     await loadStories();
-    var titles = stories.map((story) => "${story.title}. ${story.year} рік");
+    var titles = stories.map((story) => "${story.title}.");
     bot.sendMessage(
       message.chat.id,
       "Список історій",
